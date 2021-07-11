cb810fe4292ecec226d110a758c2831e28b0bf93
VijayMeena701 <govardhanmeena701@gmail.com>
2021-07-11 16:29:48 +0530

    added a basic command

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const command = require("./Commands");
 
 client.on('ready', () => {
     console.log("The Client is Ready!");
+    command(client, 'ping', message => {
+        message.channel.send("pong");
+    })
 });
 
 client.login(process.env.DISCORDJS_TOKEN);
